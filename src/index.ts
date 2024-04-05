6b8322a15ad99320ffff3ab9653764e47a396a01
Mike Elsmore <mike.elsmore@infobip.com>
2024-04-05 14:21:04 +0100

    winner picker

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import Fastify from "fastify";
 import FastifySensible from "@fastify/sensible";
 import { AuthType, Infobip } from "@infobip-api/sdk";
 import { v4 as uuid } from "uuid";
+import axios from "axios";
 
 // Fastify & it's configuration
 const fastify = Fastify({
@@ -10,13 +11,22 @@ const fastify = Fastify({
 });
 fastify.register(FastifySensible);
 
+const infobip = new Infobip({
+  baseUrl: String(process.env.INFOBIP_BASE_URL),
+  apiKey: String(process.env.INFOBIP_API_KEY),
+  authType: AuthType.ApiKey,
+});
+
+const infobipClient = axios.create({
+    baseURL: !infobip.credentials.baseUrl.indexOf('http') ? infobip.credentials.baseUrl : `https://${infobip.credentials.baseUrl}`,
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `${infobip.credentials.authorization}`,
+    },
+});
+
 // Routes
 fastify.post("/", async (request: any, reply) => {
-  const infobip = new Infobip({
-    baseUrl: String(process.env.INFOBIP_BASE_URL),
-    apiKey: String(process.env.INFOBIP_API_KEY),
-    authType: AuthType.ApiKey,
-  });
   console.log(request.body.treasureNumber);
   const response = await infobip.channels.sms.send({
     messages: [
@@ -47,6 +57,36 @@ fastify.post("/", async (request: any, reply) => {
   console.log(response)
   return {};
 });
+
+fastify.get("/pick-winner", {}, async(request: any, reply: any) => {
+    try {
+        const filterQuery = {"#and":[{"#contains": { "tags": "CityJSLondon2024" }}]};
+        const peopleResp = await infobipClient.get(`/people/2/persons?limit=1000&filter=${encodeURIComponent(JSON.stringify(filterQuery))}`, {})
+        peopleResp.data.persons.filter((person: any) => {
+            return person.customAttributes.Raffle == "Yes";
+        })
+        const { persons } = peopleResp.data;
+        const winner = persons[Math.floor(Math.random()*persons.length)]
+        const winnerNumber = winner.contactInformation.phone[0].number;
+        // const winnerNumber = '447816961602';
+        const whatsappObject = {
+            type: 'text',
+            to: winnerNumber,
+            from: process.env.INFOBIP_WHATSAPP_SENDER,
+            content: {
+              text: `Hey ${winner.firstName.split(' ')[0]}, you've won the Infobip Prize. Stand up so you can get it! If you don't say now, it'll pass to the next person.`,
+            },
+        }
+        await infobip.channels.whatsapp.send(whatsappObject);
+        return {
+            data: [winner],
+        }
+    } catch (error) {
+        console.error(error);
+        reply.status(500);
+        reply.send(error);
+    }
+})
 
 // Run the server
 const start = async () => {
